be737822ad6ea8918580f475dbb4fd09ee01f86d
bcho892 <AKLbc70@gmail.com>
2022-09-13 00:49:52 +1200

    added relevant files

---
+++ src/app/service/sanity.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import SanityClient from '@sanity/client';
+import ImageUrlBuilder from '@sanity/image-url';
+import { environment } from 'src/environments/environment';
+import { Tutor } from '../types/util';
+@Injectable({
+  providedIn: 'root'
+})
+export class SanityService {
+
+  constructor() { }
+
+  sanityClientCredidentials = {
+    option: SanityClient({
+      projectId: environment.project_id,
+      dataset: "production"
+    })
+  };
+  urlFor = (source: any) =>
+    ImageUrlBuilder(this.sanityClientCredidentials.option).image(source)
+
+
+  async getTutors(): Promise<Tutor[]> {
+    return await this.sanityClientCredidentials.option.fetch(
+      `*[_type == "tutor"]{
+        _id,
+        name,
+        "image": image.asset -> url,
+        description,
+        subjects
+      }`
+    );
+  }
+}
